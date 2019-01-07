71b6538588ac50a0bb7ab4d95ed9152678fff43b
Zhen Lab Ubuntu <zhenli0729@gmail.com>
2019-01-06 17:20:12 -0800

    updated getTable.R

--- content/getTable.R
+++ content/getTable.R
@@ -15,7 +15,23 @@ temp2 <- apply(dt[-1,], 1, function(xx){
   temp <- as.character(sapply(xx, function(yy){paste("<td>", yy, "</td>", sep="")}))
   c("<tr>", temp, "</tr>")
 })
-temp2 <- c('<table id="example" class="display" style="width:100%">"', temp, "<tbody>", temp2, "</tbody>", "</table>")
+temp2 <- c("<!DOCTYPE html>",
+             '<html>',
+             '<head>',
+             '<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">',
+             "</head>",
+             "<body>",
+           '<table id="example" class="display" style="width:100%">"', temp, "<tbody>", temp2, "</tbody>", "</table>",
+           '<script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>',
+             '<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>',
+             "<script>",
+             "$(document).ready(function(){",
+               '$("#example").dataTable();',
+             "})",
+           "</script>",
+             "</body>",
+             "</html>"
+           )
 write.table(temp2, paste("d", sheet, ".html", sep=""), eol = "\n",row.names = F,col.names = F,quote = F)
 
 ## After saving the formatted data, remember to insert the rest of the html text into the document, otherwise it won't work.
